8da2d9212bebbe51697f16daf5bfcecfe86b9040
anhson <sonanhnguyen003@gmail.com>
2024-03-03 17:02:12 +0700

    fix: geminiChat

--- server/config/gemini.js
+++ server/config/gemini.js
@@ -1,16 +1,16 @@
 // import { GoogleGenerativeAI } from '@google/generative-ai'
-const {GoogleGenerativeAI} = require('@google/generative-ai')
-require("dotenv").config();
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+require('dotenv').config();
 let geminiChat = null;
 try {
 	const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 	const gemini = genAI.getGenerativeModel({ model: 'gemini-pro' });
 	geminiChat = gemini.startChat({
 		history: [],
-	}); 
+	});
 	console.log('Init gemini success!');
 } catch (error) {
 	console.log('Error init gemini', error);
-	geminiChat = null; 
+	geminiChat = null;
 }
 module.exports = geminiChat; 
